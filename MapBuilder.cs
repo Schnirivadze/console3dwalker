7c87683441ea377f8d2988c991fc95f82024f6d3
Andrew Selezniov <andrewselezniov@gmail.com>
2023-04-20 10:10:33 +0200

    Git crashed, last stable version
    Problems with map

---
+++ MapBuilder.cs
@@ -0,0 +1,251 @@
+﻿namespace test
+{
+	internal class MapBuilder
+	{
+		struct Room
+		{
+			public int x;
+			public int y;
+			public int width;
+			public int height;
+			public bool isinroom(int x, int y)
+			{
+				return (y >= this.y && y < this.y + this.height && x >= this.x && x < this.x + this.height);
+			}
+		}
+		public int startX = -1;
+		public int startY = -1;
+		public bool[][] Map = new bool[10][];
+		public MapBuilder(int size, int rooms, int minwidthroom, int maxwidthroom, int minheightroom, int maxheightroom, bool logging)
+		{
+			if (logging) Console.WriteLine("Making map");
+			//------------------------------------------------------------------------------------------------------create map
+			Map = new bool[size][];
+			for (int i = 0; i < size; i++)
+			{
+				Map[i] = new bool[size];
+				for (int j = 0; j < Map[i].Length; j++)
+				{
+					Map[i][j] = true;
+				}
+			}
+			if (logging) Console.WriteLine("Map made");
+			//------------------------------------------------------------------------------------------------------create rooms
+			Random random = new Random();
+			Room[] Rooms = new Room[rooms];
+			for (int i = 0; i < rooms; i++)
+			{
+				int width = random.Next(minwidthroom, maxwidthroom);
+				int height = random.Next(minheightroom, maxheightroom);
+				int rx = random.Next(1, Map[0].Length - width);
+				int ry = random.Next(1, Map.Length - height);
+				Rooms[i] = new Room();
+				Rooms[i].x = rx;
+				Rooms[i].y = ry;
+				Rooms[i].width = width;
+				Rooms[i].height = height;
+				//Console.WriteLine($"x:{rx} y:{ry} w:{width} h:{height}");
+				for (int y = 0; y < height; y++)
+				{
+					for (int x = 0; x < width; x++)
+					{
+						Map[y + ry][x + rx] = false;
+					}
+				}
+
+			}
+			if (logging) Console.WriteLine("Rooms created");
+			//------------------------------------------------------------------------------------------------------make pathes
+			foreach (Room room in Rooms)
+			{
+				int radius = 1;
+				bool found = false;
+				int centerX = room.x + room.width / 2;
+				int centerY = room.y + room.height / 2;
+				int toy = 0;
+				int tox = 0;
+				while (radius < 50)
+				{
+
+
+					//top
+					for (int x = centerX - radius; x < centerX + radius; x++)
+					{
+						if (x >= 0 && x < Map[0].Length && centerY + radius >= 0 && centerY + radius < Map.Length && !room.isinroom(x, centerY + radius) && Map[centerY + radius][x] == false)
+						{
+							found = true;
+							toy = centerY + radius;
+							tox = x;
+							break;
+						}
+					}
+					if (!found)
+					{
+						//bottom
+						for (int x = centerX - radius; x < centerX + radius; x++)
+						{
+							if (x >= 0 && x < Map[0].Length && centerY - radius >= 0 && centerY - radius < Map.Length && !room.isinroom(x, centerY - radius) && Map[centerY - radius][x] == false)
+							{
+								found = true;
+								toy = centerY - radius;
+								tox = x;
+								break;
+							}
+						}
+					}
+					if (!found)
+					{
+						//left
+						for (int y = centerY - radius; y < centerY + radius; y++)
+						{
+							if (y >= 0 && y < Map.Length && centerX - radius >= 0 && centerX - radius < Map[0].Length && !room.isinroom(centerX - radius, y) && Map[y][centerX - radius] == false)
+							{
+								found = true;
+								toy = y;
+								tox = centerX - radius;
+								break;
+							}
+						}
+					}
+					if (!found)
+					{
+						//right
+						for (int y = centerY - radius; y < centerY + radius; y++)
+						{
+							if (y >= 0 && y < Map.Length && centerX + radius >= 0 && centerX + radius < Map[0].Length && !room.isinroom(centerX + radius, y) && Map[y][centerX + radius] == false)
+							{
+								found = true;
+								toy = y;
+								tox = centerX + radius;
+								break;
+							}
+						}
+					}
+					if (found)
+					{
+						break;
+					}
+					else
+					{
+						radius++;
+					}
+				}
+				if (found)
+				{
+					double tX = room.x;
+					double tY = room.y;
+					double step = 0.05;
+					double sx = (tox - room.x) * step;
+					double sy = (toy - room.y) * step;
+
+					//Console.WriteLine("Found line| Start: {0} {1}| End: {2} {3}| step: {4} {5}", room.x, room.y, tox, toy, sx, sy);
+					while (Math.Abs(tox - tX) > 1 && Math.Abs(toy - tY) > 1 && (tX > 0 && tY > 0 && tX < Map[0].Length && tY < Map.Length))
+					{
+						int rx = (int)(tX);
+						int ry = (int)(tY);
+						//Console.WriteLine("ch:{0} {1}", rx, ry);
+						for (int y = ry - 1; y < ry + 1; y++)
+						{
+							for (int x = rx - 1; x < rx + 1; x++)
+							{
+								if (x >= 0 && x < Map[0].Length && y >= 0 && y < Map.Length) Map[y][x] = false;
+
+							}
+						}
+						tX += sx;
+						tY += sy;
+
+					}
+					for (int y = toy - 1; y < toy + 1; y++)
+					{
+						for (int x = tox - 1; x < tox + 1; x++)
+						{
+							if (x >= 0 && x < Map[0].Length && y >= 0 && y < Map.Length) Map[y][x] = false;
+
+						}
+					}
+					tX += sx;
+					tY += sy;
+				}
+				else
+				{
+					//Console.WriteLine("Cant find path");
+				}
+			}
+			if (logging) Console.WriteLine("Made paths");
+			//------------------------------------------------------------------------------------------------------walls
+			{
+				for (int x = 0; x < Map[0].Length; x++)
+				{
+					Map[0][x] = true;
+				}
+				for (int x = 0; x < Map[0].Length; x++)
+				{
+					Map[Map.Length - 1][x] = true;
+				}
+				for (int y = 0; y < Map.Length; y++)
+				{
+					Map[y][0] = true;
+				}
+				for (int y = 0; y < Map.Length; y++)
+				{
+					Map[y][Map[0].Length - 1] = true;
+				}
+			}
+			if (logging) Console.WriteLine("Made walls");
+			//------------------------------------------------------------------------------------------------------Find starting point
+			for (int y = 1; y < size - 1; y++)
+			{
+				for (int x = 1; x < size - 1; x++)
+				{
+					if (!Map[y][x]) { startX = x; startY = y; break; }
+				}
+				if (startX != -1 && startY != -1) break;
+			}
+		}
+		public string[] getmap(int _x, int _y, int range, int degree)
+		{
+			char playerch = '@';
+
+			/*if (degree >= 23 && degree < 68) playerch = '↘';
+			else if (degree >= 68 && degree < 113) playerch = '↓';
+			else if (degree >= 113 && degree < 158) playerch = '↙';
+			else if (degree >= 158 && degree < 203) playerch = '←';
+			else if (degree >= 203 && degree < 248) playerch = '↖';
+			else if (degree >= 248 && degree < 293) playerch = '↑';
+			else if (degree >= 293 && degree < 337) playerch = '↗';
+			else if ((degree >= 337 && degree < 360)|| (degree >= 0 && degree < 23)) playerch = '→';*/
+
+			if (degree >= 45 && degree < 135) playerch = '←';
+			else if (degree >= 135 && degree < 225) playerch = '↑';
+			else if (degree >= 225 && degree < 315) playerch = '→';
+			else if ((degree >= 315 && degree <= 360) || (degree >= 0 && degree < 45)) playerch = '↓';
+			List<string> map = new List<string>();
+			map.Add('┌' + new string('─', (int)range * 2) + '┐');
+			map.Add('│' + new string(' ', (int)((range * 2) - 3) / 2) + "MAP " + new string(' ', (int)((range * 2) - 3) / 2) + '│');
+			map.Add('├' + new string('─', (int)range * 2) + '┤');
+			//┤ ├
+			for (double y = _y - range; y < _y + range; y++)
+			{
+				string line = "│";
+				for (double x = _x - range; x < _x + range; x++)
+				{
+
+					if (x >= 0 && y >= 0 && x < Map[0].Length && y < Map.Length)
+					{//░▒█▓
+						line += (y == _y && x == _x) ? playerch : (Map[(int)y][(int)x]) ? '▒' : ' ';
+					}
+					else
+					{
+						line += ' ';
+					}
+				}
+				line += '│';
+				map.Add(line);
+			}//└──┘
+			map.Add('└' + new string('─', (int)range * 2) + '┘');
+			return map.ToArray();
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,173 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using test;
+
+internal class Program
+{
+	#region Imports
+	[DllImport("user32.dll")]
+	static extern bool GetCursorPos(out POINT lpPoint);
+	[StructLayout(LayoutKind.Sequential)]
+	struct POINT
+	{
+		public int X;
+		public int Y;
+	}
+	#endregion
+	#region Static variables
+	static int fps = 30;
+	static MapBuilder mb = new(50, 50, 7, 10, 7, 10, false);
+	static StringBuilder OutputBuilder = new();
+	static double pX = mb.startX;
+	static double pY = mb.startY;
+	static int pD = 0;
+	static int pov = 60;
+	static double steppov = 0.5;
+	static double speed = 0.1;
+	static int viewlength = 15;
+	static int ScreenHeight = 30;
+	static bool showmap = false;
+	#endregion
+	static void Main(string[] args)
+	{
+		Console.SetWindowSize((int)(pov / steppov) + 1, ScreenHeight + 1);
+		Console.SetBufferSize((int)(pov / steppov) + 1, ScreenHeight + 1);
+		Console.CursorVisible = false;
+		Console.OutputEncoding = System.Text.Encoding.UTF8;
+		Thread curs = new Thread(new ThreadStart(CursWhatch));
+		Thread movement = new Thread(new ThreadStart(movementWhatch));
+		movement.Start();
+		curs.Start();
+		List<int> CeilingList = new List<int>();
+		List<int> FloorList = new List<int>();
+		List<double> DistanceList = new List<double>();
+		int sf = 0;
+		string[] minimap = new string[1];
+		Stopwatch stpw = new();
+		while (true)
+		{
+			stpw.Restart();
+			sf++;
+			OutputBuilder.Clear();
+			DistanceList.Clear();
+			CeilingList.Clear();
+			FloorList.Clear();
+			Console.Title = $"D:{pD}";
+			// For each column, calculate the projected ray angle into world space
+			for (double d = 0 - pov / 2; d < pov / 2; d += steppov)
+			{
+				double RayX = Math.Sin(((d + pD) / 180) * Math.PI);
+				double RayY = Math.Cos(((d + pD) / 180) * Math.PI);
+
+				double distanceToWall = 0;
+				while (distanceToWall < viewlength)
+				{
+					distanceToWall += 0.1;
+					int testX = (int)(pX + (RayX * distanceToWall));
+					int testY = (int)(pY + (RayY * distanceToWall));
+					//if out of bounds
+					if (testX < 0 || testY < 0 || testX > mb.Map.Length || testY > mb.Map.Length)
+					{
+						distanceToWall = viewlength; break;
+					}
+					else if (mb.Map[testY][testX])
+					{
+						distanceToWall *= Math.Cos(Math.Abs(d) / 180 * Math.PI);
+						break;
+					}
+
+				}
+
+				// Calculate distance to ceiling and floor
+				DistanceList.Add(distanceToWall);
+				int Ceiling = (int)((double)(ScreenHeight / 2.0) - ScreenHeight / ((double)distanceToWall));
+				CeilingList.Add(Ceiling);
+				int Floor = ScreenHeight - Ceiling;
+				FloorList.Add(Floor);
+			}
+			//Fill Outputbuilder
+			int startx = 0;
+			if (showmap) { minimap = mb.getmap((int)pX, (int)pY, 10, pD); }
+			for (int y = 0; y < ScreenHeight; y++)
+			{
+				if (showmap && y < minimap.Length) { OutputBuilder.Append(minimap[y]); startx = minimap[y].Length; }
+				else { startx = 0; }
+
+				for (int x = startx; x < (int)(pov / steppov); x++)
+				{
+					char wallshade = ' ';
+					if (DistanceList[x] <= viewlength / 4) wallshade = '█';
+					else if (DistanceList[x] < viewlength / 3) wallshade = '▓';
+					else if (DistanceList[x] < viewlength / 2) wallshade = '▒';
+					else if (DistanceList[x] < viewlength) wallshade = '░';
+					else wallshade = ' ';
+
+
+					if (y < CeilingList[x]) OutputBuilder.Append(' ');
+					else if (y >= CeilingList[x] && y < FloorList[x]) OutputBuilder.Append(wallshade);
+					else
+					{
+						double f = 1.0 - ((double)y - ScreenHeight / 2) / ((double)ScreenHeight / 2);
+						if (f < 0.25) wallshade = '#';
+						else if (f < 0.5) wallshade = 'x';
+						else if (f < 0.75) wallshade = '.';
+						else if (f < 0.9) wallshade = '-';
+						else wallshade = ' ';
+						OutputBuilder.Append(wallshade);
+					}
+				}
+				OutputBuilder.Append('\n');
+			}
+			//Output
+			Console.SetCursorPosition(0, 0);
+			Console.Write(OutputBuilder.ToString());
+			stpw.Stop();
+			if (1000 / fps > stpw.Elapsed.Milliseconds) Thread.Sleep(1000 / fps - stpw.Elapsed.Milliseconds);
+
+
+		}
+	}
+
+	private static void movementWhatch()
+	{
+		double step = speed;
+		while (true)
+		{
+			ConsoleKeyInfo key = Console.ReadKey();
+			// Convert degrees to radians
+			double angle = pD * Math.PI / 180.0;
+
+			if (key.Key == ConsoleKey.W && !mb.Map[(int)(pY + step * Math.Sin(angle))][(int)(pX + step * Math.Cos(angle))])
+			{
+				pX += step * Math.Sin(angle);
+				pY += step * Math.Cos(angle);
+			}
+			else if (key.Key == ConsoleKey.S && !mb.Map[(int)(pY - step * Math.Sin(angle))][(int)(pX - step * Math.Cos(angle))])
+			{
+				pX -= step * Math.Sin(angle);
+				pY -= step * Math.Cos(angle);
+			}
+			else if (key.Key == ConsoleKey.M)
+			{
+				showmap = !showmap;
+			}
+
+		}
+
+	}
+
+	private static void CursWhatch()
+	{
+		POINT mousePos;
+		while (true)
+		{
+			GetCursorPos(out mousePos);
+			pD = (720 * mousePos.X / 1919 > 360) ? (720 * mousePos.X / 1919) - 360 : 720 * mousePos.X / 1919;
+			Thread.Sleep(50); // Add a 50-millisecond delay
+
+		}
+	}
+
+
+}
